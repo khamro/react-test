ad735e89a7a485f04fd3dca922d6062351b13b75
khamro <khatib.amro1@gmail.com>
2017-11-10 19:53:09 +0200

    first commit with code

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Contacts from './Contacts';
+
+const app = document.getElementById('app');
+ReactDOM.render(<Contacts/>,app);
+
+
